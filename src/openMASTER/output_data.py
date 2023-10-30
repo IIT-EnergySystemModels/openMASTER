d8f4fe18e960b3b7d8d47b062fe910536540cbde
Daviddb33 <ddominguez@comillas.edu>
2023-10-30 18:53:07 +0100

    Big refactor

--- src/openMASTER/output_data.py
+++ src/openMASTER/output_data.py
@@ -9,65 +9,65 @@ import os
 # Functions
 # =========
 
-def list_to_excel_table(data, index_dim, output_path, file_name, sheet_name):
-    """
-    Converting a Dataframe containing a Output Variable from a Pyomo model into a
-    more comprehensive Excel table.
-
-    INPUTS
-    ======
-    data: Pandas Dataframe. Table containing the output variable info
-    index_dim: list. Desired dimensions for the data indexes: index_dim[0]: row index dimensions
-                                                              index_dim[1]: col index dimensions
-    output_path: str. Path to the Output folder.
-    file_name: str. Name of the Excel file to create or where to concatenate new sheets.
-    sheet_name: str. Name of the Excel sheet name where to store the variable info. Usually corresponds to the name of the variable.
-    concatenate_to_table: bool. If True, the results are concatenated to the Excel table indicated in the "output_path",
-                                but in a new Excel sheet named after the variable "name".
-
-    """
-    # Set names of the Pyomo Variable
-    set_names = list(data.columns)[:-1]
-    # Sets belonging to the rows
-    row_sets = [set_names[index] for index in range(index_dim[0])]
-    # Sets belonging to the columns
-    column_sets = [set_names[index+index_dim[0]] for index in range(index_dim[1])]
-    if index_dim[0] == 0 and index_dim[1] == 0:
-        pivoted = data.copy()
-    else:
-        # Creating a pivot table index by the row and column sets
-        pivoted = pd.pivot_table(data, index=row_sets, columns=column_sets)
-        pivoted.reset_index(inplace=True)
-
-    ## Changing the name and position of the indexes
-    if index_dim[0] > 0 and index_dim[1] > 0:
-        # Column indexes
-        col_index_tuple = pivoted.columns.names
-        new_col_index_tuple = tuple([None]*len(col_index_tuple))
-        pivoted.columns.names = new_col_index_tuple
-        # Row indexes
-        for i in range(index_dim[0]):
-            # Changing the value of the indexes
-            pivoted.columns.values[i] = tuple([""]) + tuple([col_index_tuple[j+1] for j in range(index_dim[1]-1)]) + tuple([pivoted.columns[i][0] + " / " + col_index_tuple[index_dim[1]]])
-            # Chaging the name of the indexes
-            pivoted.rename(columns = {pivoted.columns[i]: pivoted.columns.values[i]}, inplace=True)
-
-    # Eliminating the index from the pivoted table
-    pivoted.index = [""] * len(pivoted)
-    pivoted.rename(columns={"index": ""}, inplace=True)
-
-    # Excel File Path
-    output_file_path = os.path.join(output_path, file_name)
-    # Checking if the Excel file is already created
-    if os.path.exists(output_file_path):
-        with pd.ExcelWriter(output_file_path, mode='a', engine='openpyxl') as writer:
-            pivoted.to_excel(writer, sheet_name=sheet_name)
-    else:
-        pivoted.to_excel(output_file_path, sheet_name=sheet_name)
+# def list_to_excel_table(data, index_dim, output_path, file_name, sheet_name):
+#     """
+#     Converting a Dataframe containing a Output Variable from a Pyomo model into a
+#     more comprehensive Excel table.
+# 
+#     INPUTS
+#     ======
+#     data: Pandas Dataframe. Table containing the output variable info
+#     index_dim: list. Desired dimensions for the data indexes: index_dim[0]: row index dimensions
+#                                                               index_dim[1]: col index dimensions
+#     output_path: str. Path to the Output folder.
+#     file_name: str. Name of the Excel file to create or where to concatenate new sheets.
+#     sheet_name: str. Name of the Excel sheet name where to store the variable info. Usually corresponds to the name of the variable.
+#     concatenate_to_table: bool. If True, the results are concatenated to the Excel table indicated in the "output_path",
+#                                 but in a new Excel sheet named after the variable "name".
+# 
+#     """
+#     # Set names of the Pyomo Variable
+#     set_names = list(data.columns)[:-1]
+#     # Sets belonging to the rows
+#     row_sets = [set_names[index] for index in range(index_dim[0])]
+#     # Sets belonging to the columns
+#     column_sets = [set_names[index+index_dim[0]] for index in range(index_dim[1])]
+#     if index_dim[0] == 0 and index_dim[1] == 0:
+#         pivoted = data.copy()
+#     else:
+#         # Creating a pivot table index by the row and column sets
+#         pivoted = pd.pivot_table(data, index=row_sets, columns=column_sets)
+#         pivoted.reset_index(inplace=True)
+# 
+#     ## Changing the name and position of the indexes
+#     if index_dim[0] > 0 and index_dim[1] > 0:
+#         # Column indexes
+#         col_index_tuple = pivoted.columns.names
+#         new_col_index_tuple = tuple([None]*len(col_index_tuple))
+#         pivoted.columns.names = new_col_index_tuple
+#         # Row indexes
+#         for i in range(index_dim[0]):
+#             # Changing the value of the indexes
+#             pivoted.columns.values[i] = tuple([""]) + tuple([col_index_tuple[j+1] for j in range(index_dim[1]-1)]) + tuple([pivoted.columns[i][0] + " / " + col_index_tuple[index_dim[1]]])
+#             # Chaging the name of the indexes
+#             pivoted.rename(columns = {pivoted.columns[i]: pivoted.columns.values[i]}, inplace=True)
+# 
+#     # Eliminating the index from the pivoted table
+#     pivoted.index = [""] * len(pivoted)
+#     pivoted.rename(columns={"index": ""}, inplace=True)
+# 
+#     # Excel File Path
+#     output_file_path = os.path.join(output_path, file_name)
+#     # Checking if the Excel file is already created
+#     if os.path.exists(output_file_path):
+#         with pd.ExcelWriter(output_file_path, mode='a', engine='openpyxl') as writer:
+#             pivoted.to_excel(writer, sheet_name=sheet_name)
+#     else:
+#         pivoted.to_excel(output_file_path, sheet_name=sheet_name)
 #------
 
 
-def pyomo_vars_to_dataframe(path, output_path, sheetname, m_instance):
+def export_model_to_csv(path, output_path, sheetname, m_instance):
     """
     Extracting the information for each Output Variable in a Pyomo model.
 
@@ -127,7 +127,7 @@ def pyomo_vars_to_dataframe(path, output_path, sheetname, m_instance):
 #------
 
 
-def vars_csv_to_dict(path):
+def import_results_from_csv(path):
     """
     Load all the csv files corresponding to Variable data, convert them to Pandas Dataframe
     and store them in a Python dictionary.
