683b2e15c2edaffac22816490816cd7c872bd624
Manuel Perez <mperezb@comillas.edu>
2023-10-31 18:12:23 +0100

    Refactoring: graphing tools

--- scenarios/PNIEC_Greenfield_31_10_2023-17_27_47/src/openMASTER/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "1.0.0"
-from openMASTER.model import make_model
-from openMASTER.loader import load_dataportal_from_csv, excel_to_csv, load_dataportal_from_excel
-from openMASTER.output_data import export_model_to_csv, import_results_from_csv
-import openMASTER.utils
